d1ba61bd3c431d380270a8682a94c466e669a52b
sxu0 <sxu0726@gmail.com>
2022-11-02 04:15:49 -0400

    convert to main script for running solver
    remove tests

--- test_helmholtz.m
+++ test_helmholtz.m
@@ -1,63 +1,5 @@
-%% testing helmholtz_FE_solver() function
-clear; close all;
-
-mag_inc_wave = 1;
-p = 2;
-nelem = 1000;
-plt = true;
-
-figure
-for k = (11:60)/10
-[U, ~, ~] = helmholtz_FE_solver(k, mag_inc_wave, p, nelem, plt);
-end
-hold off
-
-% confirmed functional + verified output against analytical soln
-
-
-%% testing helmholtz_assemble_inprod_matrix() function
-clear; close all;
-
-p = 2;
-nelem = 1000;
-
-X = helmholtz_assemble_inprod_matrix(p, nelem);
-
-x_diag = diag(X);
-x_supdiag = diag(X, 1);
-x_subdiag = diag(X, -1);
-x_blank_diag = diag(X, 2);
-
-% confirmed functional, yet to verify
-
-
-%% testing helmholtz_off_on_setup() function
-clear; close all;
-
-mag_inc_wave = 1;
-p = 2;
-nelem = 1000;
-
-[A1, A2, A3, F] = helmholtz_off_on_setup(mag_inc_wave, p, nelem);
-
-A1_diag = diag(A1);
-A1_supdiag = diag(A1, 1);
-A1_subdiag = diag(A1, -1);
-A1_blank_diag = diag(A1, 2);
-A2_diag = diag(A2);
-A2_supdiag = diag(A2, 1);
-A2_subdiag = diag(A2, -1);
-A2_blank_diag = diag(A2, 2);
-A3_diag = diag(A3);
-A3_blank_diag = diag(A3, 1);
-
-% confirmed functional, yet to verify
-
-
-%% testing the heart of the solver!
-% (next 3 code sections)
-% helmholtz_snapshots(), helmholtz_RB_offline(), & helmholtz_RB_online()
-clear; close all
+%% running the 1D Helmholtz solver!
+clear; close all; clc;
 
 k_arr = linspace(0.1, 8, 40);
 n_train = 20;
@@ -66,11 +8,12 @@ mag_inc_wave = 1;
 p = 2;
 nelem = 1000;
 
-% plot eigenvalues & associated errors to find ideal error tolerance
-[~, ~, ~] = helmholtz_snapshots(n_train, 1, p, nelem, true, mag_inc_wave);
+% plot eigenvalues & associated errors
+% examine + set ideal error tolerance below
+[~, ~, ~] = helmholtz_snapshots(n_train, 1, p, nelem, 'log', mag_inc_wave);
 
 %% OFFLINE
-err_tol = 2.2e-7;
+err_tol = 2e-6;
 [A1_N_max, A2_N_max, A3_N_max, F1_N_max, Z_N_max, mesh, ref] = helmholtz_RB_offline( ...
     n_train, err_tol);
 
