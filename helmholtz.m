3ab5cbe2bac1fedea5875eba9e30182c16044cd6
sxu0 <sxu0726@gmail.com>
2022-11-04 04:54:21 -0400

    save output figures, add switch for saving figs

--- helmholtz.m
+++ helmholtz.m
@@ -1,5 +1,8 @@
-%% running the 1D Helmholtz solver!
+% running the 1D Helmholtz solver!
 clear; close all; clc;
+
+%% CONFIG
+save_figs = false;
 
 k_arr = linspace(0.1, 8, 40);
 n_train = 20;
@@ -8,16 +11,20 @@ mag_inc_wave = 1;
 p = 2;
 nelem = 1000;
 
+%% RB ACCURACY
 % plot eigenvalues & associated errors
 % examine + set ideal error tolerance below
 [~, ~, ~] = snapshots(n_train, 1, p, nelem, 'log', mag_inc_wave);
+if save_figs
+    print(fullfile('figs', 'sorted_eigenvalues_and_errors.png'), '-dpng')
+end
 
-%% OFFLINE
+%% OFFLINE STEPS
 err_tol = 2e-6;
 [A1_N_max, A2_N_max, A3_N_max, F1_N_max, Z_N_max, mesh, ref] = rb_offline( ...
     n_train, err_tol);
 
-%% ONLINE
+%% ONLINE STEPS
 [u_N_arr, s_N_arr, u_arr] = rb_online( ...
     A1_N_max, A2_N_max, A3_N_max, F1_N_max, Z_N_max, mag_inc_wave, k_arr);
 
@@ -25,5 +32,16 @@ err_tol = 2e-6;
 if ~exist('N', 'var')
     N = size(u_N_arr, 1);
 end
-anim_soln_real = visualize(k_arr, u_arr, mesh, ref, mag_inc_wave, n_train, N);
-anim_soln_imag = visualize(k_arr, u_arr, mesh, ref, mag_inc_wave, n_train, N, 'imag');
+if save_figs
+    real_gif = fullfile('figs', ...
+        ['soln_real_pt_ntrain=' num2str(n_train) '_N=' num2str(N) '.gif']);
+    imag_gif = fullfile('figs', ...
+    ['soln_imag_pt_ntrain=' num2str(n_train) '_N=' num2str(N) '.gif']);
+else
+    real_gif = '';
+    imag_gif = '';
+end
+anim_soln_real = visualize( ...
+    k_arr, u_arr, mesh, ref, mag_inc_wave, n_train, N, 'real', real_gif);
+anim_soln_imag = visualize( ...
+    k_arr, u_arr, mesh, ref, mag_inc_wave, n_train, N, 'imag', imag_gif);
